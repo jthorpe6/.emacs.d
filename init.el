d5922a64cdcb62302fc984ad1867b4e64ad88d2a
Joe Thorpe <joe.t@interruptlabs.co.uk>
2024-04-25 15:00:53 +0100

    ok, put it back

--- init.el
+++ init.el
@@ -868,10 +868,9 @@
 (use-package treesit-auto
   :ensure t
   :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+  (treesit-auto-install 'prompt))
+  ;; :config
+  ;; (treesit-auto-add-to-auto-mode-alist 'all))
 
 ;; eglot ----------------------------------------------------------------------------------------------
 (use-package eglot
