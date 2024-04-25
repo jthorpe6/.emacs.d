994bce81c38a0ce2bfe32498d0c8c9ca3ed226d8
Joe Thorpe <joe.t@interruptlabs.co.uk>
2024-04-25 10:38:18 +0100

    not convinced i'm actually using tree-sitter

--- init.el
+++ init.el
@@ -859,7 +859,8 @@
 ;; tree-sitter ---------------------------------------------------------------------------------------
 (use-package tree-sitter
   :ensure t
-  :config (global-tree-sitter-mode))
+  :config (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 (use-package tree-sitter-langs
   :ensure t)
