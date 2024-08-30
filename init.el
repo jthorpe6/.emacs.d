b8f5bcb469b1c1b744f94a49cbfdb6a6ba7f0d9f
Joe Thorpe <joe.t@interruptlabs.co.uk>
2024-08-30 08:10:29 +0100

    remove pyvenv

--- init.el
+++ init.el
@@ -918,19 +918,6 @@
 
 (use-package ruff-format
   :ensure t
-
-(use-package pyvenv
-  :ensure t
-  :config
-  (pyvenv-mode t)
-
-  ;; Set correct Python interpreter
-  (setq pyvenv-post-activate-hooks
-        (list (lambda ()
-                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
-  (setq pyvenv-post-deactivate-hooks
-        (list (lambda ()
-                (setq python-shell-interpreter "python3")))))
   :hook (python-base-mode . ruff-format-on-save-mode))
 
 (use-package pyenv-mode
