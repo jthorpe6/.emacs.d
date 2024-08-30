e881874feb47863d255169b30452d946a283eb8e
Joe Thorpe <joe.t@interruptlabs.co.uk>
2024-08-30 13:18:17 +0100

    remove old treesitter code

--- init.el
+++ init.el
@@ -856,14 +856,6 @@
   (setq blamer-prettify-time-p t))
 
 ;; tree-sitter ---------------------------------------------------------------------------------------
-;; (use-package tree-sitter
-;;   :ensure t
-;;   :config (global-tree-sitter-mode)
-;;   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-;; (use-package tree-sitter-langs
-;;   :ensure t)
-
 (use-package treesit-auto
   :demand t
   :custom
